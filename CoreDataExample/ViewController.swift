27c47d265bfb9b19665c9e4cb8e8eac8ae1c3791
Hoàng Khánh <khanh.ht1996@gmail.com>
2018-04-18 21:02:22 +0700

    Add feature create and save data to Core Data

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -9,7 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func btnAdd(_ sender: UIButton) {
+        
+        let department = DepartmentMO.insertNewDepartment(address: "An Example Adress", city: "Hanoi", name: "Sale department", zipCode: 123456)
+        var employees = Set<EmployeeMO>()
+        if let employee1 = EmployeeMO.insertNewEmployee(name: "Employee name 1",
+                                                        dateOfBirth: NSDate.calculateDate(day: 1, month: 1, year: 1990),
+                                                        hireDate: NSDate(),
+                                                        identifier: "A0001",
+                                                        isManager: true,
+                                                        title: "Sale manager 1") {
+            employee1.department = department
+            employees.insert(employee1)
+        }
+        
+        if let employee2 = EmployeeMO.insertNewEmployee(name: "Employee name 2",
+                                                        dateOfBirth: NSDate.calculateDate(day: 2, month: 2, year: 1990),
+                                                        hireDate: NSDate(),
+                                                        identifier: "A0002",
+                                                        isManager: false,
+                                                        title: "Sale manager 2") {
+            employee2.department = department
+            employees.insert(employee2)
+        }
+        
+        if let employee3 = EmployeeMO.insertNewEmployee(name: "Employee name 3",
+                                                        dateOfBirth: NSDate.calculateDate(day: 3, month: 3, year: 1991),
+                                                        hireDate: NSDate(),
+                                                        identifier: "A0003",
+                                                        isManager: true,
+                                                        title: "Sale manager 3") {
+            employee3.department = department
+            employees.insert(employee3)
+        }
+        department?.addToEmployees(employees as NSSet)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
